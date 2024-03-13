9132f0174457f4db8fff612c28ee612d48f9f632
Lord Turmoil <tony-turmoil@outlook.com>
2024-03-13 12:12:28 +0800

    feat: add NoSleep.js

--- js/metronome.js
+++ js/metronome.js
@@ -400,3 +400,10 @@ function initWaveform() {
     initPlay();
     initWaveform();
 })();
+
+// enable no sleep
+var noSleep = new NoSleep();
+document.addEventListener('click', function enableNoSleep() {
+    document.removeEventListener('click', enableNoSleep, false);
+    noSleep.enable();
+}, false);

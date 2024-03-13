d68450665459f8114e42e755c520967622fc7617
Lord Turmoil <tony-turmoil@outlook.com>
2024-03-13 12:12:39 +0800

    update: deploy using .ftpignore

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,2 @@
 @echo off
-
-mkdir temp
-@REM copy css, js, res, fonts folder into temp
-xcopy /s /e /y /q css temp\css\
-xcopy /s /e /y /q js temp\js\
-xcopy /s /e /y /q res temp\res\
-xcopy /s /e /y /q fonts temp\fonts\
-@REM copy index.html into temp
-xcopy /q index.html temp
-@REM copy publish
-call PseudoFTP.Client transfer -s temp -i Metronome --overwrite
-rmdir /s /q temp
+call PseudoFTP.Client transfer -s .\ -l Metronome --overwrite
